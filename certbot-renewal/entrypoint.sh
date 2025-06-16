73de374a8468446241decc9c719afc93539fff2d
Nick Aguilar <nickaguilarh@gmail.com>
2025-06-16 13:19:24 -0500

    fix: update script paths for proper execution in container
    Update Dockerfile to copy scripts into a dedicated directory to improve organization.
    Modify entrypoint script to reference script paths relative to the new location.
    Change ensures scripts are found and executed as intended within the container environment.
    No breaking changes.
    
    Signed-off-by: Nick Aguilar <nickaguilarh@gmail.com>

--- certbot-renewal/entrypoint.sh
+++ certbot-renewal/entrypoint.sh
@@ -37,7 +37,7 @@ fi
 
 # Run certificate check - if renewal is needed, the script exits 0; else exits 1
 echo "### [INFO] --- Phase 1: Renewal check ---"
-00-check.sh
+./scripts/00-check.sh
 CHECK_RENEW_STATUS=$?
 if [ $CHECK_RENEW_STATUS -ne 0 ]; then
   echo "### [INFO] Certificate is still valid, renewal NOT required. Exiting process."
@@ -46,7 +46,7 @@ fi
 
 # If renewal is needed, run Certbot
 echo "### [INFO] --- Phase 2: Run Certbot issuance/renewal ---"
-01-renew.sh
+./scripts/01-renew.sh
 CERTBOT_STATUS=$?
 if [ $CERTBOT_STATUS -ne 0 ]; then
   echo "### [FATAL] Certbot phase failed with status $CERTBOT_STATUS. Aborting."
@@ -55,7 +55,7 @@ fi
 
 # Upload to Vault
 echo "### [INFO] --- Phase 3: Upload certificate/key to Vault ---"
-02-upload.sh
+./scripts/02-upload.sh
 UPLOAD_STATUS=$?
 if [ $UPLOAD_STATUS -ne 0 ]; then
   echo "### [FATAL] Upload to Vault phase failed with status $UPLOAD_STATUS. Aborting."
